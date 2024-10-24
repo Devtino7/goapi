1d2343901511747ba9282e8754f4e474c1656f1d
Devtino <vitormachadopersonal@outlook.com>
2024-10-24 15:18:10 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "github.com/Devtino7/goapi/router"
+import (
+	"github.com/Devtino7/goapi/config"
+	"github.com/Devtino7/goapi/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
 
+	// Inicialize config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config inicialization error: %v", err)
+		return
+	}
 	// Inicialize router
 	router.Inicialize()
 }
